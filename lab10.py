e4580776e3b775536a1791b8814952e6b6e01545
yanooomm <115118650+yanooomm@users.noreply.github.com>
2023-04-08 18:49:04 +0500

    Add files via upload

---
+++ lab10.py
@@ -0,0 +1,26 @@
+# 1. вывести главное окно по центру
+# 2. отключить от него resize
+# 3. после главной кнопки появляется три новые кнопки ( через toplevel)
+# фейерверк у главного окна по кнопке. еще кнопка которая отключает
+# через 15, 30, 45 секунд с обратным отсчетом.
+# через  это время фейерверк заканчивается, форма закрывается
+
+from tkinter import*
+root = Tk()
+#form = Toplevel
+width = 350
+height = 150
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+x = int((screen_width/2) - (width/2))
+y = int((screen_height/2) - (height/2))
+root.geometry(str(width) + "x" + str(height) + "+" + str(x) + "+" + str(y))
+root.title("lab10")
+button = Button(root, height = 1, width = 7, text = "Старт", command = lambda: click_button(button))
+button.pack()
+def click_button(button):
+    button1 = Button(root, height = 1, width = 7, text = "15 секунд")
+    button2 = Button(root, height = 1, width = 7, text = "30 секунд")
+    button["text"] = "45 секунд"
+root.resizable(False, False)
+root.mainloop()
